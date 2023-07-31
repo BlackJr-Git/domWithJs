9103228acb98ad094d18814e25ca5afd6a993898
BlackJr-Git <juniorassosa@gmail.com>
2023-07-31 16:57:51 +0100

    Feat: add a button for a small progress bar

--- app-test.js
+++ app-test.js
@@ -4,6 +4,8 @@ const compteur = document.getElementById("compteur");
 const buttonReset = document.getElementById("btn-reset")
 
 
+
+
 function add() {
   compteur.innerText = parseInt(compteur.innerText) + 1;
 }
@@ -21,3 +23,17 @@ function reset(){
 buttonReset.addEventListener("click", reset)
 buttonAdd.addEventListener("click", add);
 buttonRemove.addEventListener("click", remove);
+
+
+const compteur2 = document.getElementById("compteur2");
+const btnStart = document.getElementById("btn-start")
+
+function ajout(){
+  // for (let index = 0 ; index < 70 ; index++) {
+  //   compteur2.style.width = parseInt(compteur2.style.width) + 1
+  // }
+
+  compteur2.style.width = '70%'
+}
+
+btnStart.addEventListener('click', ajout)
