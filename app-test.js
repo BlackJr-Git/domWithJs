ff57041e18cb124911ec1ac66c1d01e9f4c81b75
BlackJr-Git <juniorassosa@gmail.com>
2023-08-03 14:06:30 +0100

    feat : changing the add button using .onclick methode

--- app-test.js
+++ app-test.js
@@ -3,10 +3,7 @@ const buttonAdd = document.getElementById("btn-add");
 const compteur = document.getElementById("compteur");
 const buttonReset = document.getElementById("btn-reset");
 
-function add() {
-  compteur.innerText = parseInt(compteur.innerText) + 1;
-  compteur.style.transition = "10s";
-}
+
 
 function remove() {
   if (compteur.innerText > 0 || compteur.innerText == "") {
@@ -18,36 +15,27 @@ function reset() {
   compteur.innerText = 0;
 }
 
+buttonAdd.onclick = function add() {
+  compteur.innerText = parseInt(compteur.innerText) + 1;
+  compteur.style.transition = "10s";
+} ;
+
 buttonReset.addEventListener("click", reset);
-buttonAdd.addEventListener("click", add);
+// buttonAdd.addEventListener("click", add);
 buttonRemove.addEventListener("click", remove);
 
 const compteur2 = document.getElementById("compteur2");
 const btnStart = document.getElementById("btn-start");
 
-function ajout() {
+function ajout(compteur2) {
   // for (let i = 10; i <= 70; i++) {
   //   compteur2.style.width = compteur2.style.width = `${i}%`;
   // }
-
   compteur2.style.width = compteur2 + '%'
-
-  
-
-  // compteur2.style.transitionDelay = "10s";
-
-  // let cpt;
-  // if (cpt === 0){
-  //   compteur2.style.width = "70%";
-  //   cpt = 1;
-  // } else {
-  //   compteur2.style.width = "10%";
-  //   cpt = 0;
-  // }
 }
 
 setTimeout(function () {
-  updateajout(50);
+  ajout(50);
 }, 2000);
 
 btnStart.addEventListener("click", ajout);
@@ -63,3 +51,4 @@ setTimeout(function() {
   updateProgressBar(70);
 }, 2000);
  
+
